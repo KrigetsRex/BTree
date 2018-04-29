785d330b653023f32ef231c8c6856360b1911d2d
Adam Keener <keener101@gmail.com>
2018-04-29 10:18:22 -0600

    BTree works, need to do freq and traverse/search functions

--- BTree.java
+++ BTree.java
@@ -1,92 +1,265 @@
-/*****************************************
- *  BTree
- *  CS321
- *  Class for container type BTree
- *  Contains the methods pertaining to 
- *  creating and editing a BTree
- ****************************************/
- 
- 
- import java.util.List;
- 
- /**
-  * BTree Class
-  */
-  public class BTree{
-	  //Private Variables
-	  private int t;  //the degree of the BTree
-	  private int MAXELEMENTS;
-	  private int MAXCHILDREN;
-	  private BTreeNode root;
-	  
-	  //Public Methods
-	  
-	  /**
-	   * Constructor
-	   */
-	  public BTree(int degree){
-		  this.t = degree;
-		  this.MAXELEMENTS = (2*t) - 1;
-		  this.MAXCHILDREN = MAXELEMENTS + 1;
-	  }
-	  
-	  /**
-	   * add key to a node in the tree
-	   * @param key: the element to be added to the tree
-	   */
-	  public void addElement(Object key){
-		  
-		if (root == null){
-			root = new BTreeNode(key);
-		}
-		else if ((root.getChildren().length() == 0) && (root.getReference().length() < MAXELEMENTS)){
-			root.addReference(key);
-		}
-		else {
-			addRecursive(root, key);
-		}				 
-	  }
-	  
-	  //Private Methods
-	  /**
-	   * recursive method to find the leaf in which the key should reside and add it there
-	   * if leaf is full then it will promote the appropriate key and add it to the parent 
-	   * node via a recursive call to promote.
-	   * @param node: the node in which to add the element
-	   * @param key: the element which is to be added to the node
-	   */
-	  private void addRecursive(BTreeNode node, Object key){
-		//end case - cur node is a leaf
-		if ((node.getChildren().length() == 0){
-			if (node.getReference().length() < MAXELEMENTS)){
-				node.addReference(key);
+
+/**
+ * BTree Class for CS321
+ * 
+ * @author Keener
+ * @author Luke Grice
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BTree {
+
+	Node root;
+	int t;   							//minimum degree
+	int maxKeys;
+	int maxChildren;
+
+	public BTree(int t){
+		this.t = t;
+		root = null;
+
+		this.maxChildren = 2 * t;
+		this.maxKeys =  maxChildren - 1;
+		
+		
+		
+
+	}
+	
+	/*
+	 * Inserts a key into the Btree
+	 * 
+	 * k - sequence containing a long of binary bases
+	 */
+
+	public void insert(Sequence k){
+		
+		System.out.println("INSERT: " + k.getBases());
+		
+		
+		// special case - empty tree
+		if (root == null){			
+			root = new Node(true);
+			root.keys.add(k);
+		}else{
+			
+		//non-empty trees
+			
+			//if root is full
+			if (root.keys.size() == maxKeys){
+				
+				//creates a new node to make as root
+				Node newRoot = new Node(false);
+				
+				//adds the current root as a child
+				newRoot.children.add(root);      
+				
+				//splits the full root
+				newRoot.splitChild(0, root);
+				
+				//determines child position for new sequence
+				int i = 0;
+				if (newRoot.keys.get(0).getBases() < k.getBases()){
+					i++;
+				}
+				
+				//adds sequence to specified child
+				newRoot.children.get(i).insertNonFull(k);
+				
+				//re-assigns root
+				root = newRoot;
+				
+				
+			} else {
+				//if root is not full, determine proper position of sequence
+		
+				root.insertNonFull(k);
 			}
-			else{ 
-				//get middle element
-				//add key
-				//remove middle element from node
-				//promote middle element to parent node
+			
+
+		}
+		
+	}
+	
+	//currently not working, should traverse the tree starting at the root
+	
+	public void fullTraverse(){
+		root.traverse();
+	}
+	
+	/*
+	 * Returns the root node
+	 */
+	
+	public Node getRoot(){
+		return root;
+	}
+	
+
+	/*
+	 * Inner class for nodes
+	 */
+
+	//class is currently public - making it private made some testing difficult. Not sure if there's a workaround
+	public class Node{
+		ArrayList<Sequence> keys;
+		ArrayList<Node> children;
+		boolean isLeaf;
+
+		/*
+		 * Constructor
+		 */
+		
+		public Node(boolean isLeaf){
+			this.isLeaf = isLeaf;
+			keys = new ArrayList<Sequence>();
+			children = new ArrayList<Node>();
+
+		}
+		
+		/*
+		 * Method to insert a given key into this node. Should only be called if a node is not full
+		 */
+
+		
+		public void insertNonFull(Sequence k){
+						
+			//get the right-most element index
+			int i = keys.size() - 1;
+			
+			//if leaf, determine the correct position of the new key
+			if (isLeaf){
+				while (i >= 0 && keys.get(i).getBases() > k.getBases()){
+					i--;
+				}
+				//insert key at location
+				keys.add(i+1, k);
+			} else {
+				//if not leaf, find child that will get new key
+				
+				while (i >= 0 && keys.get(i).getBases() > k.getBases()){
+					i--;
+				}
+				
+				//check if that child is full
+				if (children.get(i+1).keys.size() == maxKeys){
+					
+					//if so, split the child
+					splitChild(i+1, children.get(i+1));
+					
+					
+					//after split, check which new branch is going to have new key
+					if (keys.get(i+1).getBases() < k.getBases()){
+						i++;
+					}
+					
+
+				}
+				
+				children.get(i+1).insertNonFull(k);
 			}
 		}
-		//else check key against existing keys and call addRecursive(correct-child-node, key)
-	  }
-	  
-	  /**
-	   * recursive method to promote an element up the tree
-	   * @param node: the node in which the promoted key will attempt to occupy
-	   * @param key: the key to be promoted
-	   */
-	  private void promoteRecursive(BTreeNode node, Object key){
-		  //end case
-		  if node.getReference().length() < MAXELEMENTS{
-			  node.addReference(key);
-		  }
-		  else {
-			  //get middle key - which will need to be promoted
-			  //add passed in key to list of elements
-			  //take all elements < middle-key and make new node - attach this new node to current node.parent,
-			  //and take all elements > middle-key and make new node - attach this new node to current node.parent,
-			  //promoteRecursive(node.getParent(), middleKey);
-		  }
-	  }
-  }
+		
+		
+		/*
+		 * Splits a full node, using i as the split index
+		 */
+		
+		public void splitChild(int i, Node fullNode){
+
+			//make a new node to contain some of fullnodes keys
+			Node newNode = new Node(fullNode.isLeaf);
+			
+			//copy last t-1 keys of fullnode into the newnode
+			for (int j = 0; j < t -1; j++){
+				newNode.keys.add(fullNode.keys.get(j+t));
+			}
+			
+			//copy the last t children of fullnode into newnode
+			
+			if (!fullNode.isLeaf){
+				for (int j = 0; j < t; j++){
+					newNode.children.add(fullNode.children.get(j+t));
+				}
+				
+				//remove old nodes that have now been transfered
+				
+				for (int o = 0; o < t; o++){
+					fullNode.children.remove(fullNode.children.size()-1);
+				}
+				
+			}
+			
+			
+			
+			//link new child to node
+			
+			this.children.add(i+1, newNode);
+			
+			
+			//add the middle key of full node into the new node
+			this.keys.add(i, fullNode.keys.get(t-1));
+			
+			// remove old keys that have now been split
+			for (int o = fullNode.keys.size()-1; o>= t-1 ; o--){
+				fullNode.keys.remove(o);
+			}
+		}
+		
+		/*
+		 * Should search the btree when called on the node. 
+		 * 
+		 * Currently untested.
+		 */
+		
+		public Node search(Sequence k){
+			
+			int i = 0;
+			
+			while (i < keys.size() && k.getBases() > keys.get(i).getBases()){
+				i++;
+			}
+			
+			if (keys.get(i).getBases() == k.getBases()){
+				return this;
+			}
+			
+			if (isLeaf){
+				return null;
+			}
+			
+			return children.get(i).search(k);
+		}
+		
+		public void traverse(){
+			int i;
+			
+			System.out.println("tr start");
+			
+			for (i = 0; i < keys.size(); i++){
+				if(!isLeaf){
+					
+					
+					children.get(i).traverse();
+					
+				}
+			}
+			
+			if (!isLeaf){
+				children.get(i).traverse();
+			}
+			
+		}
+		
+		public ArrayList<Sequence> getKeys() {
+			return keys;
+		}
+		
+		
+		
+
+
+	}
+
+}
