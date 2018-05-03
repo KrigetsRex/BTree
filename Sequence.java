62ab44445d9f293a5a591c03d6a739f4e70b8605
Tavi Kohn <tavikohn@u.boisestate.edu>
2018-05-03 00:15:29 -0600

    Add B-Tree serialization, searching, and frequency counting.

--- Sequence.java
+++ Sequence.java
@@ -1,25 +1,34 @@
-public class Sequence {
+import java.io.*;
 
-	private long bases;
-	private int freq;
-	
-	public Sequence(long bases){
-		this.bases = bases;
-			
+/**
+ * Wrapper for a long that serializes more efficiently
+ */
+public class Sequence implements Comparable<Sequence>, Serializable {
+    public long sequence;
+
+    public Sequence(long sequence) {
+        this.sequence = sequence;
+    }
+
+    @Override
+    public int compareTo(Sequence sequence) {
+        return (int)(this.sequence - sequence.sequence);
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.writeLong(sequence);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException {
+        sequence = in.readLong();
+    }
+
+	private void readObjectNoData() throws ObjectStreamException {
+		sequence = 0;
 	}
-	
-	public long getBases() {
-		return bases;
-	}
-	public void setBases(long bases) {
-		this.bases = bases;
-	}
-	
-	public int getFreq(){
-		return freq;
-	}
-	
-	public void incrementFreq(){
-		freq++;
-	}
+
+    @Override
+    public String toString() {
+        return Long.toString(sequence);
+    }
 }
